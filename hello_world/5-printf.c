00842bee9591267bf8cbdca50808d69a2d84d8c5
glovek08 <gbrielbbarn@gmail.com>
2025-02-14 17:44:39 +0000

    [betty] 5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: returns 0 if correct.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
