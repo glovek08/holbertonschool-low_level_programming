245607b54729809f809e4086d9d4a057cbc424df
glovek08 <gbrielbbarn@gmail.com>
2025-02-14 17:29:34 +0000

    [betty] 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 if correct.
+ */
 int main(void)
 {
+	/* writes a string to the std output */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
