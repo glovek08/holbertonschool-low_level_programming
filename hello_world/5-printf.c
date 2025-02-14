84de6b01bcbf0a660f1e398682226738cd59a0fe
glovek08 <gbrielbbarn@gmail.com>
2025-02-14 17:52:18 +0000

    [betty] 5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -5,8 +5,8 @@
  * Return: returns 0 if correct.
  */
 int main(void)
-{	
+{
 	/* prints a string to the standard output */
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
