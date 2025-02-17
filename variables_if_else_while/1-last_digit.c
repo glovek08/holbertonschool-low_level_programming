aeef8c244744e7f4a32eea44ea02df57870d0418
glovek08 <gbrielbbarn@gmail.com>
2025-02-17 14:11:29 +0000

    [fix] 1-last_digit.c removed trying to use strings concat as a normal human being, C is for aliens

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,31 +1,32 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+
 /**
- * main - Entry point
- *
- * Return: returns 0 if succesful.
- */
+* main - Entry point
+*
+* Return: returns 0 if succesful.
+*/
 
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	int lastDigit = abs(n) % 10;
-	string msg = "Last digit of %i is", n;
-	if (lastDigit > 5)
-	{
-		msg += " and is greater than 5\n";
-	}
-	else if (lastDigit == 0)
-	{
-		msg += " and is 0\n";
-	}
-	else if (lastDigit < 6 && lastDigit != 0)
-	{
-		msg += " and is less than 6 and not 0\n";
-	}
-	printf(msg);
-	return (0);
+int n;
+int lastDigit;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+lastDigit = n % 10;
+if (lastDigit > 5)
+{
+	printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 }
+else if (lastDigit == 0)
+{
+	printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+}
+else
+{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+}
+return (0);
+}
+
