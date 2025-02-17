1dca14f7d0c89ba47d552a6e712308efb6686004
glovek08 <gbrielbbarn@gmail.com>
2025-02-17 17:44:10 +0000

    [feat] 8-print_base16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: returns 0 if succesful.
+*/
+
+int main(void)
+{
+	int digitCode = 48;
+
+	while (digitCode <= 102)
+	{
+		putchar(digitCode);
+		digitCode++;
+		if (digitCode == 57)
+		{
+			digitCode = 97;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
