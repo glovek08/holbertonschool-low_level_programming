020a3edbf6c75dbaf1c6f0569663415f903b5266
glovek08 <gbrielbbarn@gmail.com>
2025-02-18 19:49:51 +0000

    [feat] 4-isalpha, 4-main.c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic characters.
+ * @c: the character.
+ *  Return: 0 if "c" is a letter, 0 otherwise.
+ */
+
+int _isalpha(int c)
+{
+	if (c <= 65 && c >= 90)
+	{
+		if (c <= 97 && c >= 122)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
