a6b9adff6561369a65ebbff30ef52ad84c11ca99
glovek08 <gbrielbbarn@gmail.com>
2025-02-19 18:39:35 +0000

    [betty] 8-24_hours.c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,35 +7,35 @@
  */
 int jack_bauer(void)
 {
-    int hour, minute;
+	int hour, minute;
 
-    for (hour = 0; hour < 24; hour++)
-    {
-        for (minute = 0; minute < 60; minute++)
-        {
-            if (hour < 10)
-            {
-                _putchar('0');
-                _putchar(hour + '0');
-            }
-            else
-            {
-                _putchar((hour / 10) + '0');
-                _putchar((hour % 10) + '0');
-            }
-            _putchar(':');
-            if (minute < 10)
-            {
-                _putchar('0');
-                _putchar(minute + '0');
-            }
-            else
-            {
-                _putchar((minute / 10) + '0');
-                _putchar((minute % 10) + '0');
-            }
-            _putchar('\n');
-        }
-    }
-    return (0);
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			if (hour < 10)
+			{
+				_putchar('0');
+				_putchar(hour + '0');
+			}
+			else
+			{
+				_putchar((hour / 10) + '0');
+				_putchar((hour % 10) + '0');
+			}
+			_putchar(':');
+			if (minute < 10)
+			{
+				_putchar('0');
+				_putchar(minute + '0');
+			}
+			else
+			{
+				_putchar((minute / 10) + '0');
+				_putchar((minute % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+	return (0);
 }
