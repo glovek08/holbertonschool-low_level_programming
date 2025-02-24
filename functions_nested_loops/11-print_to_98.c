d17bf175c1c0c52ed89bd9b4ae6c83e982d67543
glovek08 <gbrielbbarn@gmail.com>
2025-02-24 16:13:53 +0000

    [fix] correct syntax, deleted option to catch >999 numbers in 11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,36 +1,22 @@
-#include <main.h>
-
+#include "main.h"
 /**
  * print_to_98 - prints all natural numbers to selected target.
- * @baseNum: the starter number.
+ * @starterNum: the number from which to count up or down.
  * Return: void.
  */
-
-void print_to_98(int baseNum)
+void print_to_98(int starterNum)
 {
 	int TARGET_NUM = 98;
 
-	char number_too_long[25] = "ERROR (NUMBER TOO LONG)";
-
-	if (starterNum > 999) /* Catches four digit numbers */
-	{
-		int i;
-		for (i = 0; i < (sizeof(number_too_long) / sizeof(number_too_long[0])); i++)
-		{
-			_putchar(number_too_long[i]);
-		}
-		return;
-	}
 	if (starterNum > 98)
 	{
-		do
-		{
+		do {
 			if (starterNum > 99) /* Handles three digit numbers */
 			{
 				_putchar((starterNum / 100) + '0');
 			}
 			_putchar(((starterNum / 10) % 10) + '0');
-			putchar('0' + (starterNum % 10));
+			_putchar('0' + (starterNum % 10));
 			if (starterNum != TARGET_NUM)
 			{
 				_putchar(',');
@@ -38,19 +24,21 @@ void print_to_98(int baseNum)
 			}
 			starterNum--;
 		}
-		while(targetNum >= TARGET_NUM)
+	while (starterNum >= TARGET_NUM);
 	}
-	else (starterNum < 98)
+	else if (starterNum < 98)
 	{
-		if (starterNum < 0) /* ADD LOGIC TO HANDLE NEGATIVE NUMBERS */
+		if (starterNum < 0)
 		{
-
+			_putchar('-');
+			_putchar((-starterNum / 10) + '0');
+			_putchar((-starterNum % 10) + '0');
 		}
-		else (starterNum > 9) /* Handles two digit numbers */
+		else if (starterNum > 9)
 		{
-			_putchar(((starterNum / 10) % 10) + '0');
+			_putchar('0' + ((starterNum / 10) % 10));
 		}
-		putchar('0' + (starterNum % 10));
+		_putchar('0' + (starterNum % 10));
 		if (starterNum != TARGET_NUM)
 		{
 			_putchar(',');
