22cd009fec9418c6522d8dc7a04e155d9d2c3b85
glovek08 <gbrielbbarn@gmail.com>
2025-02-24 16:20:57 +0000

    [refactor] shortened 11-print_to_98.c to less than 40 lines

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,49 +1,34 @@
 #include "main.h"
+
 /**
- * print_to_98 - prints all natural numbers to selected target.
- * @starterNum: the number from which to count up or down.
- * Return: void.
+ * print_to_98 - Prints all natural numbers from starterNum to 98.
+ * @starterNum: The starting number.
  */
 void print_to_98(int starterNum)
 {
-	int TARGET_NUM = 98;
+	int target = 98;
 
-	if (starterNum > 98)
-	{
-		do {
-			if (starterNum > 99) /* Handles three digit numbers */
-			{
-				_putchar((starterNum / 100) + '0');
-			}
-			_putchar(((starterNum / 10) % 10) + '0');
-			_putchar('0' + (starterNum % 10));
-			if (starterNum != TARGET_NUM)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			starterNum--;
-		}
-	while (starterNum >= TARGET_NUM);
-	}
-	else if (starterNum < 98)
+	while (starterNum != target)
 	{
 		if (starterNum < 0)
 		{
 			_putchar('-');
-			_putchar((-starterNum / 10) + '0');
+			_putchar((-starterNum / 10) + '0' * (-starterNum >= 10));
 			_putchar((-starterNum % 10) + '0');
 		}
-		else if (starterNum > 9)
+		else
 		{
-			_putchar('0' + ((starterNum / 10) % 10));
+			if (starterNum >= 10)
+				_putchar((starterNum / 10) + '0' * (starterNum >= 100));
+			if (starterNum >= 100)
+				_putchar(((starterNum / 10) % 10) + '0');
+			_putchar((starterNum % 10) + '0');
 		}
-		_putchar('0' + (starterNum % 10));
-		if (starterNum != TARGET_NUM)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
-		starterNum++;
+		_putchar(',');
+		_putchar(' ');
+		starterNum += (starterNum < target) ? 1 : -1;
 	}
+	_putchar('9');
+	_putchar('8');
 }
+
