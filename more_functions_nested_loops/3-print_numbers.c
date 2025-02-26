25977b3223835fdcb28826031c122ad5d80fc3bf
glovek08 <gbrielbbarn@gmail.com>
2025-02-26 00:36:19 +0000

    [betty] 3-print_numbers.c

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_numbers: print the numbers from 0 to 9, followed by a new line.
+ * print_numbers - print the numbers from 0 to 9, followed by a new line.
  * Return: void.
  */
 void print_numbers(void)
