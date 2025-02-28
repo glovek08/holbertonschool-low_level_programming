e5155f586e505c182cf3f1c27c82109e1c30417c
glovek08 <gbrielbbarn@gmail.com>
2025-02-28 17:02:05 +0000

    [feat] 0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "mian.h"
+/**
+ * reset_to_98 - updates the value of a pointer's target.
+ * @targetPointer: the target pointer.
+ * Result: void.
+ */
+void reset_to_98(int *targetPointer)
+{
+	*targetPointer = 98;
+}
