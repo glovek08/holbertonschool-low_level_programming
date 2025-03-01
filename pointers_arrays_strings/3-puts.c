a8b6a7a54a4af7adefda12365bdc6ed727884ce9
glovek08 <gbrielbbarn@gmail.com>
2025-03-01 19:09:13 +0000

    [feat] 3-puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts - prints a giving string to the standard output.
+ * @str: the given string.
+ * Return: void.
+ */
+void _puts(char *str)
+{
+_puts(str);
+return;
+}
