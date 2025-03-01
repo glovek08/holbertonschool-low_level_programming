377f14f7f025fceb8c8da33d3cb082748e724956
glovek08 <gbrielbbarn@gmail.com>
2025-03-01 19:53:03 +0000

    [feat] 4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - prints a given string in reverse by its pointer.
+ * @str: the given string.
+ * Return: void.
+ */
+void print_rev(char *str)
+{
+char *strEnd = str;
+
+while (*strEnd)
+end++;
+while (end > str)
+{
+end--;
+_putchar(*end);
+}
+_putchar('\n');
+return;
+}
