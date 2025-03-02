72466f3cd3d2234e4e13aff1b5dae03ccabb4808
glovek08 <gbrielbbarn@gmail.com>
2025-03-02 22:30:01 +0000

    [feat] 5-string_toupper.c

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - uppercases all letter of a string.
+ * @
+ *
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	for (; str[i] != '\0'; i++)
+	{
+		if (str[i] > 96 && str[i] < 123)
+		{
+			str[i] -= 32;
+		}
+	}
+	return (str);
+}
