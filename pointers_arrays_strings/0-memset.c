1a8d0b25227a20ac90f3833016345cf00dab283b
glovek08 <gbrielbbarn@gmail.com>
2025-03-03 19:18:31 +0000

    [feat] 0-memset.c

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.c"
+/**
+ * _memset - fills memory with a constant byte.
+ * @str: the string to be filled.
+ * @b: the constant byte.
+ * @num: the first number of bytes to be filled.
+ * Return: pointer to @str.
+ */
+char *_memset(char *str, char b, unsigned int num)
+{
+char *temp_str = str;
+unsigned int i = 0;
+
+while (i < num)
+{
+*str = b;
+str++;
+i++;
+}
+return (temp_str);
+}
