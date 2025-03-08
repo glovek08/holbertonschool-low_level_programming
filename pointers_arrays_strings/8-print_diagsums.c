88ba726f3ba63f5f82f0fc60d60861e73e7e80c4
glovek08 <gbrielbbarn@gmail.com>
2025-03-08 17:29:51 +0000

    [fix] correct formula for B diagonal in 8-print_diagonal.c

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -17,7 +17,7 @@ void print_diagsums(int *arr, int size)
 	}
 	for (; i < size; i++)
 	{
-		diag_B += arr[i * size + (size - i)];
+		diag_B += diag_A + *((arr + i * size) + (size  - i));
 	}
 	printf("%i, %i", diag_A, diag_B);
 }
