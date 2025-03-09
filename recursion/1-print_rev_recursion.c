ad5eff547e9cd0ec2934abbc1ac1622b62fe5403
glovek08 <gbrielbbarn@gmail.com>
2025-03-09 16:23:52 +0000

    [feat] 1-print_rev_recursion.c

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints a given string in reverse.
+ * @str: the string to be printed.
+ * Return: void.
+ */
+void _print_rev_recursion(char *str)
+{
+	if (!*str)
+	{
+		_putchar('\n');
+		return;
+	}
+	_print_rev_recursion(++str);
+	_putchar(*str);
+}
