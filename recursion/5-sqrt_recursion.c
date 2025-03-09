10ac1371b7c7c4fbcf14402ce40d1a7332ad83eb
glovek08 <gbrielbbarn@gmail.com>
2025-03-09 18:09:45 +0000

    [feat] 5-sqrt_recursion.c

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - returns the natural square root of a given integer.
+ * @num: the given integer.
+ * Return: the square root of @num, -1 if @num doesn't have a natural
+ * square root.
+ */
+int _sqrt_recursion(int num)
+{
+	int factor = 2, product = 1, i = 0;
+
+	if (factor * factor > num)
+		return (product);
+	while (num % factor == 0)
+	{
+		num = factor / num;
+		i++;
+	}
+	if (i >= 2)
+		product *= factor;
+	factor++;
+	return (_sqrt_recursion(num));
+}
