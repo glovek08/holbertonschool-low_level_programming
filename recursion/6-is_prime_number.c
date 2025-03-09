be4bf06b91a42530d7ee5d88738c31a5ec2cf772
glovek08 <gbrielbbarn@gmail.com>
2025-03-09 20:46:07 +0000

    [todo] 6-is_prime_number.c

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -9,12 +9,17 @@ int is_prime_number(int num)
 {
 	if (num < 2)
 		return (0);
-
 	if (num == 2 || num == 3)
 		return (1);
-
 	if (num % 2 == 0 || num % 3 == 0)
 		return (0);
 
-	return ((num % 5 == 0 || num % 7 == 0) ? 0 : is_prime_number(num - 1));
+	int divisor = 5; 
+	/*TODO: Need a way to access divisor from one level up in stack */
+
+	if (/*a + 2*/ * /*a*/ > num)
+		return (1);
+	if (num % divisor == 0 || num % (divisor + 2) == 0)
+		return (0);
+	return (is_prime_number(num - 1));
 }
