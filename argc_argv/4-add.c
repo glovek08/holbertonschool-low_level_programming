0bd48e5b202cda80628064cd9366c3d8b3146e02
glovek08 <gbrielbbarn@gmail.com>
2025-03-10 01:42:31 +0000

    [feat] 4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include "stdio.h"
+/**
+ * main - prints the result of adding two positive numbers.
+ * @argc: arguments counter.
+ * @argv: arguments vector.
+ * Return: 0 if successful. 1 if error.
+ */
+int main(int argc, char **argv)
+{
+	int result = 0;
+	int num = 0;
+	int i = 0, j = 0;
+	char *arg = '\0';
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	while (i < argc)
+	{
+		arg = argv[i];
+		while (arg[j])
+		{
+			num *= 10 + (arg[j] - '0');
+			j++;
+		}
+		result += num;
+		i++;
+	}
+	printf("%i\n", result);
+	return (0);
+}
