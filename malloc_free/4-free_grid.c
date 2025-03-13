baecc56671eada371c4e43e78860c88172fa288b
Gabriel Barnada <131347941+glovek08@users.noreply.github.com>
2025-03-12 21:56:32 -0300

    [refactor] 4-free_grid.c

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -11,12 +11,7 @@ void free_grid(int **grid, int height)
 {
 	int i = 0;
 
-	if (!grid)
-		return;
-
-	for (; i < height; i++)
-	{
-		free(grid[i]);
-	}
+	if (!grid) return;
+	for (; i < height; i++) free(grid[i]);
 	free(grid);
 }
