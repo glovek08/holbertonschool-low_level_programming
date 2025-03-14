3fe5425d7609154f2e05b2bb3bd6e3a6a94d49c4
glovek08 <gbrielbbarn@gmail.com>
2025-03-14 18:27:06 +0000

    [feat] 0-malloc_checked.c

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @size: the size of the new memory block.
+ * Return: pointer to the newly allocated memory.
+ */
+void *malloc_checked(unsigned int size)
+{
+	char *mem_block = malloc(size * sizeof(*mem_block));
+
+	if (!mem_block)
+		return (NULL);
+	return (mem_block);
+}
