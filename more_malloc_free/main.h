379584e2812e4670136c61cb9a5ace7d42ac1db5
glovek08 <gbrielbbarn@gmail.com>
2025-03-14 17:09:05 +0000

    [feat] header file, main files for all functions

---
+++ more_malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef TEST_H
+#define TEST_H
+
+int *array_range(int, int);
+
+char *string_nconcat(char *, char *, unsigned int);
+
+void *malloc_checked(unsigned int);
+void *_calloc(unsigned int, unsigned int);
+
+
+#endif /* TEST_H */
