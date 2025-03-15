86de005c34f2fe617876aa08962b55bde2056017
Gabriel Barnada <131347941+glovek08@users.noreply.github.com>
2025-03-14 22:42:04 -0300

    [refactor] 3-array_range.c

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -11,18 +11,11 @@ int *array_range(int start, int end)
 {
 	int *arr_range  = (void *)0;
 	int i = 0, length = 0;
-
-	if (start > end)
-		return (NULL);
-
+	
+	if (start > end) return (NULL);
 	length = end - start + 1;
-
 	arr_range = malloc(sizeof(int) * length);
-	if (!arr_range)
-		return (NULL);
-
-	for (; i < length; i++)
-		arr_range[i] = start + i;
-
+	if (!arr_range) return (NULL);
+	for (; i < length; i++) arr_range[i] = start + i;
 	return (arr_range);
 }
