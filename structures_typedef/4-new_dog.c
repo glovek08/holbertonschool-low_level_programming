d0379b6fb5a49c8129327d0e91374d30346fd98f
glovek08 <gbrielbbarn@gmail.com>
2025-03-15 23:29:31 +0000

    [feat] 4-new_god.c

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+/**
+ * new_dog - creates a new god.
+ * @name: the dog's name.
+ * @age: the dog's age.
+ * @owner: the dog's owner.
+ *
+ * Return: a new god.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *doggy;
+	char *name_cpy = name, *owner_cpy = owner;
+
+	doggy = malloc(sizeof(dog_t));
+	if (!doggy)
+		return (NULL);
+
+	doggy->name = name_cpy;
+	doggy->age = age;
+	doggy->owner = owner_cpy;
+
+	return (doggy);
+}
