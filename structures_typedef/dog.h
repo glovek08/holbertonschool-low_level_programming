759d400d4de390dfcf418be448e22e20875ab25e
glovek08 <gbrielbbarn@gmail.com>
2025-03-15 18:07:19 +0000

    [feat] dog structure in dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,20 @@
 #ifndef TEST_H
 #define TEST_H
 
-struct dog 
+/**
+ * struct dog - generate a new dog.
+ * @name: the dog's name.
+ * @age: the dog's age.
+ * @owner: who owns the dog.
+ *
+ * Description: Generates a new dog structure.
+ */
+
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
 #endif /* TEST_H */
