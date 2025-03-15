562e47a7e396777eba866c30584360b4c4b8d91d
Gabriel Barnada <131347941+glovek08@users.noreply.github.com>
2025-03-14 22:44:42 -0300

    [refactor] 1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -17,34 +17,16 @@ char *string_nconcat(char *dest, char *src, unsigned int n)
 	char *new_str = (void *)0;
 	unsigned int dest_len = 0, src_len = 0, i = 0;
 
-	if (!dest)
-		dest = "";
-	if (!src)
-		src = "";
-
-	while (src[src_len])
-		src_len++;
-
-	while (dest[dest_len])
-		dest_len++;
-
-	if (n > src_len)
-		n = src_len;
-
+	if (!dest) dest = "";
+	if (!src) src = "";
+	while (src[src_len]) src_len++;
+	while (dest[dest_len]) dest_len++;
+	if (n > src_len) n = src_len;
 	new_str = malloc(dest_len + n + 1);
-	if (!new_str)
-		return (NULL);
-
-	for (; i < dest_len; i++)
-		new_str[i] = dest[i];
-
-	for (i = 0; i < n; i++)
-	{
-		new_str[dest_len + i] = src[i];
-	}
-
+	if (!new_str) return (NULL);
+	for (; i < dest_len; i++) new_str[i] = dest[i];
+	for (i = 0; i < n; i++) new_str[dest_len + i] = src[i];
 	new_str[dest_len + n] = '\0';
-
 	return (new_str);
 
 }
