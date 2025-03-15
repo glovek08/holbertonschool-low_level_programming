b99a6816a42e08fe125c0200d0671471fd8bf1d2
glovek08 <gbrielbbarn@gmail.com>
2025-03-15 00:50:51 +0000

    [feat] 3-array_range.c

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * array_range - creates an array of integers containing a range of numbers.
+ * @start: the starting number.
+ * @end: the ending number.
+ *
+ * Return: the pointer to the new array, 'NULL' on error.
+ */
+int *array_range(int start, int end)
+{
+	int *arr_range  = (void *)0;
+	int i = 0;
+
+	if (start > end)
+		return (NULL);
+
+	arr_range = malloc(sizeof(int) * (start + end + 1));
+	if (!arr_range)
+		return (NULL);
+
+	for (; i < (start + end + 1); i++)
+	{
+		arr_range[i] = start + i;
+	}
+
+	return (arr_range);
+}
