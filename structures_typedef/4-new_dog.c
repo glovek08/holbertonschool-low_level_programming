0efc7070bbdf0ede41a1b20d835180448bc68246
glovek08 <gbrielbbarn@gmail.com>
2025-03-16 00:00:41 +0000

    [fix] cannot use strlen in 4-new_god.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,6 +2,21 @@
 #include <string.h>
 #include "dog.h"
 /**
+ * _strlen - Calculates the length of a string manually
+ * @str: Input string
+ *
+ * Return: Length of the string (excluding null terminator)
+ */
+int _strlen(const char *str)
+{
+	int len = 0;
+
+	while (str[len])
+		len++;
+	return (len);
+}
+
+/**
  * new_dog - creates a new god.
  * @name: the dog's name.
  * @age: the dog's age.
@@ -14,26 +29,27 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *doggy;
 	char *name_cpy, *owner_cpy;
 
-	name_cpy = malloc(strlen(name) + 1);
-	if (!name_cpy)
+	doggy = malloc(sizeof(dog_t));
+	if (!doggy)
 		return (NULL);
 
-	owner_cpy = malloc(strlen(owner) + 1);
+	name_cpy = malloc(_strlen(name) + 1);
+	if (!name_cpy)
+	{
+		free(doggy);
+		return (NULL);
+	}
+
+	owner_cpy = malloc(_strlen(owner) + 1);
 	if (!owner_cpy)
 	{
 		free(name_cpy);
+		free(doggy);
 		return (NULL);
 	}
+
 	strcpy(name_cpy, name);
 	strcpy(owner_cpy, owner);
-
-	doggy = malloc(sizeof(dog_t));
-	if (!doggy)
-	{
-		free(name_cpy);
-		free(owner_cpy);
-		return (NULL);
-	}
 
 	doggy->name = name_cpy;
 	doggy->age = age;
