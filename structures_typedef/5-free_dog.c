8c4118c8313b53f41133d22e70ffde32a0104845
glovek08 <gbrielbbarn@gmail.com>
2025-03-16 00:09:22 +0000

    [feat] 5-free_dog.c

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - frees all dogs!.
+ * @d: the dog struct to be freed.
+ *
+ * Return: void.
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
