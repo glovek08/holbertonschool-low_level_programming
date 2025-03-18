f082f8d1e858b5b3e1b7d0ccad444bdca568366c
glovek08 <gbrielbbarn@gmail.com>
2025-03-18 14:36:53 +0000

    [fix] handle cases for NULL and 0 as second argument when dividing in 3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,6 +1,7 @@
 #include "function_pointers.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  * main - Entry point.
  * @argc: argument count.
@@ -12,14 +13,18 @@ int main(int argc, char **argv)
 {
 	int (*op_func)(int, int);
 	int num1, num2;
+	char *operator = argv[2];
 
 	(void)argc;
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 
+	if ((operator[0] == '/' || operator[0] == '%') && num2 == 0)
+		return (98);
+
 	op_func = get_op_func(argv[2]);
 
 	printf("%d\n", op_func(num1, num2));
-	return (0);
+	return (EXIT_SUCCESS);
 }
