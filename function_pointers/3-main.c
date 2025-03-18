d132b31c3cd1d18ca8c5936ce421664d58b62bd2
glovek08 <gbrielbbarn@gmail.com>
2025-03-18 14:24:55 +0000

    [feat] 3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,11 +1,25 @@
 #include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
+ * main - Entry point.
+ * @argc: argument count.
+ * @argv: argument vector.
  *
- *
- *
- *
+ * Return: 0 on success.
  */
-int main(void)
+int main(int argc, char **argv)
 {
+	int (*op_func)(int, int);
+	int num1, num2;
+
+	(void)argc;
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	op_func = get_op_func(argv[2]);
+
+	printf("%d\n", op_func(num1, num2));
 	return (0);
 }
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -3,9 +3,16 @@
 
 #include <stddef.h>
 
+typedef struct op
+{
+    char *op;
+    int (*f)(int, int);
+} op_t;
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 
+int (*get_op_func(char *s))(int, int);
 int int_index(int *array, int size, int (*cmp)(int));
 int op_add(int a, int b);
 int op_sub(int a, int b);
