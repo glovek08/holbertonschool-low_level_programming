96cb5ae60dd12581b76790f54fa8ae78150ab6ae
glovek08 <gbrielbbarn@gmail.com>
2025-03-19 19:35:59 +0000

    [fix] wrong 'nums' and 'count' var names

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -3,19 +3,22 @@
 /**
  * sum_them_all - returns the sum of all of its parameters.
  * @count: the count of parameters.
- * 
+ *
  * Return: the sum of all the parameters.
  */
-int sum_them_all(const unsigned int count, ...);
+int sum_them_all(const unsigned int count, ...)
 {
-	va_list nums;
-	int result = 0, i = 0;
+        va_list nums;
+        unsigned int i = 0;
+        int result = 0;
 
-	if (!n)
-		return(EXIT_FAILURE);
-	va_start(nums, n); /* Initializing with the last named arg */
-	for (; i < count; i++)
-		result += va_arg(nums, int);
-	va_free(nums);
-	return (result);
+        if (!count)
+                return (0);
+        va_start(nums, count); /* Initializing with the last named arg */
+        for (; i < count; i++)
+                result += va_arg(nums, int);
+        va_end(nums);
+
+        return (result);
 }
+
