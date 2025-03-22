f940876d33b0e69832b25e90d4cff8660a044b3b
glovek08 <gbrielbbarn@gmail.com>
2025-03-22 16:59:13 +0000

    [feat] 3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,21 +1,42 @@
-#include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
+
 /**
- * print_all - Prints values based on the format string.
- * @format: List of types of arguments passed to the function.
- *           c: char, i: int, f: float, s: char* (prints (nil) if NULL).
- *           Any other character is ignored.
- *
- * This function uses recursion to process the format string and
- * print the corresponding arguments.
+ * print_all - Prints anything based on the format string.
+ * @format: A list of types of arguments passed to the function.
+ *          c: char, i: integer, f: float, s: char * (if NULL, print (nil)).
+ *          Any other char is ignored.
  */
 void print_all(const char * const format, ...)
 {
 	va_list args;
+	int i = 0;
+	char *s;
 
 	va_start(args, format);
-	
+
+	while (format && format[i])
+	{
+		if (i > 0)
+			printf(", ");
+
+		if (format[i] == 'c')
+			printf("%c", (char)va_arg(args, int));
+		else if (format[i] == 'i')
+			printf("%d", va_arg(args, int));
+		else if (format[i] == 'f')
+			printf("%f", (float)va_arg(args, double));
+		else if (format[i] == 's')
+		{
+			s = va_arg(args, char *);
+			if (s == NULL)
+				printf("(nil)");
+			else
+				printf("%s", s);
+		}
+
+		i++;
+	}
 
 	va_end(args);
 	printf("\n");
