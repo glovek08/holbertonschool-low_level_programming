73e9d4810515c470139ca82963dc0c1d98c0d609
glovek08 <gbrielbbarn@gmail.com>
2025-03-26 14:31:28 -0300

    [fix] using switch in 3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -13,10 +13,38 @@
 void print_all(const char * const format, ...)
 {
 	va_list args;
+	int i = 0;
+	char *str;
 
 	va_start(args, format);
-	
+
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%i", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (str)
+					printf("%s", str);
+				else
+					printf("(nil)");
+				break;
+			default:
+				break;
+		}
+		i++;
+	}
 
 	va_end(args);
 	printf("\n");
 }
+
