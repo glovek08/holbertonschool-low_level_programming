486b56bdf0252b7479990aa3512f9f307f49b4f0
glovek08 <gbrielbbarn@gmail.com>
2025-04-03 18:30:54 +0000

    [fix] Fix missing newline when printing (nil) in 0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -14,7 +14,11 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (!h->str)
-			printf("[0] (nil)");
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+			continue;
+		}
 		printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		node_count++;
