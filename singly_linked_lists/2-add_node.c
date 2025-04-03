bfbfc686ae6fe6159a36d9acf9d79884a8e414bd
glovek08 <gbrielbbarn@gmail.com>
2025-04-03 19:12:19 +0000

    [feat] 2-add_node.c

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * add_node - adds a node to a given list.
+ * @head: the first node of the list.
+ * @str: the value to be insterted into the node.
+ *
+ * Return: the address of the new element, 'NULL' on failure.
+ */
+list_t *add_node(list_t, **head, const char *str)
+{
+
+}
