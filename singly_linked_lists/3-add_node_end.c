385645418494c5002735bd44355919e8aec3d533
glovek08 <gbrielbbarn@gmail.com>
2025-04-04 21:35:27 -0300

    [fix] handle strdup fail case in 3-add_node_end.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -21,6 +21,11 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (!new_node)
 		return (NULL);
 	new_node->str = strdup(str);
+	if (!new_node->str)
+	{
+		free(new_node);
+		return (NULL);
+	}
 	new_node->len = _strlen(new_node->str);
 	new_node->next = NULL;
 	if (!*head)
