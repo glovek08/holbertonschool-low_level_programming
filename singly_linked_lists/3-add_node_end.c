925e4ee294dd32d865d2b29775a34be8a6988fe4
glovek08 <gbrielbbarn@gmail.com>
2025-04-04 21:26:49 -0300

    [fix] always returning NULL on 3-add_node_end.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -31,7 +31,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	for (temp = *head; temp; temp = temp->next)
 		;
 	temp->next = new_node;
-	return (NULL);
+	return (new_node);
 }
 /**
  * _strlen - returns the length of a given string.
