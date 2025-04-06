ff44973f14b23cbf26f86dfb3749ed570e163661
glovek08 <gbrielbbarn@gmail.com>
2025-04-06 17:56:52 -0300

    [feat] 0binary_to_uint.c

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: the string containig the binary value.
+ *
+ * Return: the converted number, 0 on failure.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	int str_length = _strlen(b), i = 0;
+
+	for (; i < str_length; i++)
+	{
+		result <<= 1;
+		if (b[i] == 1)
+			result |= 1;
+	}
+	return (result);
+}
+/**
+ * _strlen - returns the length of a given string.
+ * @str: the given string.
+ *
+ * Return: the amount of characters in @str.
+ */
+unsigned int _strlen(const char *str)
+{
+	unsigned int str_len = 0;
+
+	if (!str)
+		return (str_len);
+	while (str[str_len])
+		str_len++;
+	return (str_len);
+}
