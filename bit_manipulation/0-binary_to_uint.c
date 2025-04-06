bac94eaaa91d14c893612e817a85a736b5654342
glovek08 <gbrielbbarn@gmail.com>
2025-04-06 18:06:02 -0300

    [fix] wrong conditional operator in 0-binary_to_uint.c

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -13,7 +13,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (; i < str_length; i++)
 	{
-		if (b[i] != '0' || b[i] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		result <<= 1;
 		if (b[i] == '1')
