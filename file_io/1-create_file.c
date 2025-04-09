5b22f66be7d61b19462d150f85692962eecdb217
glovek08 <gbrielbbarn@gmail.com>
2025-04-09 17:22:03 -0300

    [fix] forget to close file descriptor on null text_content case in 1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -22,7 +22,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	if (!text_content)
+	{
+		close(fd);
 		return (1);
+	}
 	written_count = write(fd, text_content, _strlen(text_content));
 	if (written_count == -1)
 	{
